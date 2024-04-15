7a7fc6120e8561730c657315fd722b9117379ee1
Jin Young Bang <jybang@bu.edu>
2024-04-14 21:45:26 -0400

    refactor: style updates

--- src/app/checkin/[id]/page.tsx
+++ src/app/checkin/[id]/page.tsx
@@ -9,9 +9,9 @@ export default function Checkin() {
   const { data: session } = useSession();
 
   return (
-    <div className="flex min-h-screen flex-col justify-center px-6 sm:px-12 md:px-24 lg:px-32 xl:px-48 2xl:px-64">
-      <div className="flex items-center justify-end">
-        <Button variant="link" className="mb-2" type="button" onClick={() => signOut(({ callbackUrl: "https://bupct.com/" }))}>Logout</Button>
+    <div className="flex min-h-screen flex-col justify-center px-6 sm:px-12 md:px-24 lg:px-32">
+      <div className="absolute top-0 right-0 mt-6 mr-2 sm:mr-6 md:mr-12">
+        <Button variant="link" type="button" onClick={() => signOut(({ callbackUrl: "https://bupct.com/" }))}>Logout</Button>
       </div>
       <h1 className="text-4xl font-bold text-gray-900 dark:text-white mb-2">Hi, {session?.user?.name}!</h1>
       <h3 className="text-md font-light text-gray-600 dark:text-white mb-6">Welcome to Phi Chi Theta, Zeta Chapter's Rush! We're excited to have you here.</h3>
@@ -20,7 +20,7 @@ export default function Checkin() {
         <div className="flex items-center space-x-2 mt-1 mb-8">
           <Input type="text" placeholder="Code" />
         </div>
-        <Button className="" type="submit">Submit</Button>
+        <Button className="w-24" type="submit">Submit</Button>
       </div>
     </div>
   );
