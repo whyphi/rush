6b7dd099dcb89aa3e4a7f19ae0800a405d9ca6af
William De Rocco <93288641+wderocco8@users.noreply.github.com>
2024-07-25 16:55:38 -0400

    added more styling to EventCard component

--- src/components/dashboard/EventCard.tsx
+++ src/components/dashboard/EventCard.tsx
@@ -15,12 +15,26 @@ export default function EventCard({
   const eventDeadline = new Date(event.deadline);
   const isEventPassed = eventDeadline < new Date();
 
+  const handleEventClick = () => {
+    if (!isEventPassed) {
+      router.push(`/checkin/${event._id}`)
+    }
+  }
+
   return (
     <Card
-      className="cursor-pointer"
-      imgAlt={event.eventCoverImageName}
-      imgSrc={event.eventCoverImage}
-      onClick={() => router.push(`/checkin/${event._id}`)}
+      className={`${isEventPassed ? "bg-gray-200 cursor-not-allowed" : "cursor-pointer"}`}
+      renderImage={() => (
+        <div className="relative">
+          <img
+            className="rounded-t-lg"
+            alt={event.eventCoverImageName}
+            src={event.eventCoverImage}
+          />
+          {isEventPassed && <div className="absolute inset-0 bg-gray-200 opacity-50 rounded-t-lg"></div>}
+      </div>
+      )}
+      onClick={handleEventClick}
     >
         <h5 className="flex items-center text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
           <span className={`flex w-2 h-2 me-3 ${isEventPassed ? "bg-red-500" : "bg-green-500"} rounded-full`}></span>
