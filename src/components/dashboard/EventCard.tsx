e685a29b88b067ad93fb9f2da8a10295718945f5
William De Rocco <93288641+wderocco8@users.noreply.github.com>
2024-07-25 15:30:50 -0400

    added status icon depending on if rush event is passed due

--- src/components/dashboard/EventCard.tsx
+++ src/components/dashboard/EventCard.tsx
@@ -11,14 +11,19 @@ export default function EventCard({
   event
 }: EventCardProps) {
   const router = useRouter();
+
+  const eventDeadline = new Date(event.deadline);
+  const isEventPassed = eventDeadline < new Date();
+
   return (
-    <Card 
-      className="p-0 gap-0 cursor-pointer"
+    <Card
+      className="cursor-pointer"
       imgAlt={event.eventCoverImageName}
       imgSrc={event.eventCoverImage}
       onClick={() => router.push(`/checkin/${event._id}`)}
     >
-        <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+        <h5 className="flex items-center text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+          <span className={`flex w-2 h-2 me-3 ${isEventPassed ? "bg-red-500" : "bg-green-500"} rounded-full`}></span>
           {event.name}
         </h5>
         <h3 className="text-base font-bold tracking-tight text-gray-900 dark:text-white">
