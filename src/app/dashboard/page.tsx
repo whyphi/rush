ffc2a1ea473c1191b6667e489db8ef539723e606
William De Rocco <93288641+wderocco8@users.noreply.github.com>
2024-07-25 20:57:03 -0400

    sending POST to backend

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -21,7 +21,11 @@ export default function Dashboard({ params }: { params: { id: string } }) {
     
     const fetchRushEvents = async () => {
       try {
-        const response = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/events/rush/default/${email}`);
+        const response = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/events/rush/default`, {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({ email: email })
+        });
         if (!response.ok) {
           // router.push("/checkin/error");
           setError(new Error("Failed to fetch event"));
@@ -42,10 +46,8 @@ export default function Dashboard({ params }: { params: { id: string } }) {
   }
   return (
     <div className="mt-20 mx-4 lg:mx-10 lg:justify-center lg:items-center">
-      {/* <div className="px-4 grid md:grid-cols-2 lg:grid-cols-3 gap-10"> */}
       <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10">
         {rushCategory?.events.map((event, index) => (
-          // <div key={index} className="max-w-lg">
           <div key={index} className="col-span-1">
             <EventCard event={event}/>
           </div>
